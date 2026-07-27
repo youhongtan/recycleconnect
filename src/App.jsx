4ae2a0eaa689beb0cae56b01bb16020ced95de70
Y Hong <youhong.tyh@gmail.com>
2026-07-27 07:31:56 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import ScrollToTop from './components/ScrollToTop';
 import SiteLayout from '@/components/layout/SiteLayout';
 import AdminLayout from '@/components/admin/AdminLayout';
+import Login from '@/pages/Login';
+import Register from '@/pages/Register';
+import ForgotPassword from '@/pages/ForgotPassword';
+import ResetPassword from '@/pages/ResetPassword';
 import Home from '@/pages/Home';
 import Learn from '@/pages/Learn';
 import Pollution from '@/pages/Pollution';
